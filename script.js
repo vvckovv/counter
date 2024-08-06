6d6cbecc4df401986ae74b35994d7dc69261eda7
vvckovv <oloyedeope14@gmail.com>
2024-08-06 14:10:16 +0100

    edited Css and Javascript file

--- script.js
+++ script.js
@@ -3,26 +3,21 @@ const addBtn = document.getElementById('add-btn');
 const minusBtn = document.getElementById('minus-Btn');
 const resetBtn = document.getElementById('reset-btn');
 
-
 let score = 0;
 
 document.getElementById("add-btn").onclick = function() {
     score++;
     scoreCount.textContent = score;
-    if (score.textContent >= 1){
-        minusBtn.style.backgroundColor = '#D926A9'
-    }   
 };
 
 document.getElementById("minus-btn").onclick = function () {
-    score--;
+    if (score > 0) {
+        score --;
+    }
     scoreCount.textContent = score;
 };
 
 document.getElementById("reset-btn").onclick = function () {
     score = 0;
     scoreCount.textContent = score;
-    if (countScore.textContent < 1){
-        subBtn.style.backgroundColor = ''
-    }
 };
